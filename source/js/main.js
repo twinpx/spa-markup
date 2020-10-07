b9be562e2875ba8f77195267a9e5d235931964d1
Tatiana <djembuka@gmail.com>
2020-10-07 11:32:50 +0300

    Header

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,27 @@
 //placeholders
 setTimeout( function() {
-  $( '.b-ph-block, h1' ).addClass( 'i-ph-animated' );
+  $( '.ph-block, h1' ).addClass( 'ph-block--animated' );
 }, 500 );
+
+
+//header dropdown
+document.querySelector( '.b-header__menu-icon' ).addEventListener( 'click', function(e) {
+  e.preventDefault();
+  $( '.b-header__dropdown-wrapper' ).slideToggle();
+  return false;
+});
+
+//header scroll
+if ( window.matchMedia( '(min-width: 576px)' ).matches ) {
+  var headerTop = document.querySelector( '.b-header__top' );
+  window.addEventListener( 'scroll', function(e) {
+    var height = headerTop.offsetHeight;
+    if ( window.scrollY >= height ) {
+      document.querySelector( 'body' ).classList.add( 'header-fixed' );
+    } else {
+      document.querySelector( 'body' ).classList.remove( 'header-fixed' );
+    }
+  });
+  
+  window.dispatchEvent( new Event( 'scroll' ));
+}
