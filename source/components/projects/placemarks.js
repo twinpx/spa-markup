54d7fcf52dfd4a5f5447f85c1bbe70d584353191
djembuka <djembuka@gmail.com>
2020-10-13 16:00:50 +0300

    Projects effects

---
+++ source/components/projects/placemarks.js
@@ -0,0 +1,130 @@
+window.projectsPlacemarks = {
+  
+  "infrastructure": [
+    {
+      "coords": [59.936,30.3],
+      "src": "/template/images/print-icon.svg",
+      "num": "10",
+      "name": "Ресторан Апрель"
+    },
+    {
+      "coords": [59.935,30.3],
+      "src": "/template/images/print-icon.svg",
+      "num": "10",
+      "name": "Детский сад"
+    },
+    {
+      "coords": [59.934,30.3],
+      "src": "/template/images/print-icon.svg",
+      "num": "10",
+      "name": "Салон красоты"
+    },
+    {
+      "coords": [59.933,30.3],
+      "src": "/template/images/print-icon.svg",
+      "num": "10",
+      "name": "Спорт зал"
+    },
+    {
+      "coords": [59.932,30.3],
+      "src": "/template/images/print-icon.svg",
+      "num": "10",
+      "name": "Автомойка"
+    }
+  ],
+  
+  "highrise": [
+    {
+      "id": "456",
+      "coords": [59.938,30.3],
+      "num": "81",
+      "heading": "Многоэтажное здание",
+      "props": "235,5 м<sup>2</sup><br>Секция 2<br>63 000 000 руб",
+      "href": "/pages/price/?type=highrise&id=81"
+    },
+    {
+      "id": "457",
+      "coords": [59.9385,30.3005],
+      "num": "82",
+      "heading": "Многоэтажное здание",
+      "props": "235,5 м<sup>2</sup><br>Секция 3<br>63 000 000 руб",
+      "href": "/pages/price/?type=cottage&id=82"
+    }
+  ],
+  
+  "lowrise": [
+    {
+      "id": "456",
+      "coords": [59.9375,30.2995],
+      "num": "83",
+      "heading": "Малоэтажное здание",
+      "props": "235,5 м<sup>2</sup><br>Секция 2<br>63 000 000 руб",
+      "href": "/pages/price/?type=highrise&id=83"
+    },
+    {
+      "id": "457",
+      "coords": [59.937,30.299],
+      "num": "84",
+      "heading": "Малоэтажное здание",
+      "props": "235,5 м<sup>2</sup><br>63 000 000 руб",
+      "href": "/pages/price/?type=cottage&id=84"
+    }
+  ],
+  
+  "townhouse": [
+    {
+      "id": "456",
+      "coords": [59.9384,30.3004],
+      "num": "85",
+      "heading": "Таунхаус",
+      "props": "235,5 м<sup>2</sup><br>Секция 2<br>63 000 000 руб",
+      "href": "/pages/price/?type=highrise&id=85"
+    },
+    {
+      "id": "457",
+      "coords": [59.9385,30.3005],
+      "num": "86",
+      "heading": "Таунхаус",
+      "props": "235,5 м<sup>2</sup><br>63 000 000 руб",
+      "href": "/pages/price/?type=cottage&id=86"
+    }
+  ],
+  
+  "cottage": [
+    {
+      "id": "456",
+      "coords": [59.9386,30.3006],
+      "num": "87",
+      "heading": "Коттедж",
+      "props": "235,5 м<sup>2</sup><br>Секция 2<br>63 000 000 руб",
+      "href": "/pages/price/?type=highrise&id=87"
+    },
+    {
+      "id": "457",
+      "coords": [59.9387,30.3007],
+      "num": "88",
+      "heading": "Коттедж",
+      "props": "235,5 м<sup>2</sup><br>63 000 000 руб",
+      "href": "/pages/price/?type=cottage&id=88"
+    }
+  ],
+  
+  "duplex": [
+    {
+      "id": "456",
+      "coords": [59.9388,30.3008],
+      "num": "89",
+      "heading": "Дуплекс",
+      "props": "235,5 м<sup>2</sup><br>Секция 2<br>63 000 000 руб",
+      "href": "/pages/price/?type=highrise&id=89"
+    },
+    {
+      "id": "457",
+      "coords": [59.9389,30.3009],
+      "num": "90",
+      "heading": "Дуплекс",
+      "props": "235,5 м<sup>2</sup><br>63 000 000 руб",
+      "href": "/pages/price/?type=cottage&id=90"
+    }
+  ]
+};
--- source/components/projects/script.js
+++ source/components/projects/script.js
@@ -4,9 +4,202 @@
   
   $( function() {
     
-    $( '#bx-panel-hider' ).bind( 'click', function() {
-      var $headerPanel = $( '.b-header-panel' );
-      $header.data( 'topBorder', $header.offset().top );
+    window.addEventListener( 'scroll', function(e) {
+      
+      if ( window.matchMedia( '(min-width: 576px)' ).matches ) {
+        document.querySelectorAll( '.b-projects__row' ).forEach( function( elem, index ) {
+          if ( elem.className.search( 'b-projects__row--shown' ) !== -1 ) {
+            return;
+          }
+          
+          if ( $( elem ).offset().top <= window.scrollY + window.outerHeight - 150 ) {
+            elem.classList.add( 'b-projects__row--shown' );
+          }
+        });
+      } else {
+        document.querySelectorAll( '.b-projects__item' ).forEach( function( elem, index ) {
+          if ( elem.className.search( 'b-projects__item--shown' ) !== -1 ) {
+            return;
+          }
+          
+          if ( $( elem ).offset().top <= window.scrollY + window.outerHeight - 50 ) {
+            elem.classList.add( 'b-projects__item--shown' );
+          }
+        });
+      }
+      
+    });
+    
+    setTimeout( function() {
+      window.dispatchEvent( new Event( 'scroll' ) );
+    }, 500);
+    
+    ymaps.ready(function () {
+      var projectsYmap = new ymaps.Map( 'projectsYmap', {
+              center: window.projectsYmapCenter,
+              zoom: window.projectsYmapZoom,
+              controls: [ 'zoomControl', 'typeSelector' ]
+          }, {
+            yandexMapDisablePoiInteractivity: true
+          }),
+          placemarkLayouts = {},
+          balloonLayouts = {},
+          buildingObj = {},
+          
+          balloonProps = {
+            build: function () {
+              this.constructor.superclass.build.call(this);
+              this._$element = $('.b-balloon', this.getParentElement());
+              this.applyElementOffset();
+
+              this._$element.find('.b-balloon__close').on('click', $.proxy(this.onCloseClick, this));
+            },
+            clear: function () {
+                this._$element.find('.b-balloon__close').off('click');
+                this.constructor.superclass.clear.call(this);
+            },
+            applyElementOffset: function () {
+              this._$element.css({
+                left: -(this._$element[0].offsetWidth / 2),
+                top: -(this._$element[0].offsetHeight)
+              });
+            },
+            onCloseClick: function (e) {
+              e.preventDefault();
+              this.events.fire('userclose');
+            },
+             /**
+             * Проверяем наличие элемента (в ИЕ и Опере его еще может не быть).
+             * @function
+             * @private
+             * @name _isElement
+             * @param {jQuery} [element] Элемент.
+             * @returns {Boolean} Флаг наличия.
+             */
+            _isElement: function (element) {
+              return element && element[0] && element.find('.arrow')[0];
+            }
+          },
+          
+          clusterers = {},
+          placemarks = {};
+          
+      projectsYmap.events.add( 'boundschange', function(e) {
+        if ( e.get( 'newZoom' ) !== e.get( 'oldZoom' )) {
+          showHideInfrastructure();
+        }
+      });
+          
+      projectsYmap.behaviors.disable( 'scrollZoom' ); 
+      projectsYmap.events.add( 'click', function (e) {
+        projectsYmap.balloon.close();
+      });
+      
+      if ( window.planPlacemarks ) {
+        
+        for ( var buildingType in window.planPlacemarks ) {
+          
+          if ( buildingType === 'infrastructure' ) {
+            
+            //create placemark layout
+            placemarkLayouts[ buildingType ] = ymaps.templateLayoutFactory.createClass( '<div class="b-placemark-icon i-' + buildingType + '" style="background-image: url( $[properties.src] );"></div>' );
+            
+            //create balloon layout
+            balloonLayouts[ buildingType ] = ymaps.templateLayoutFactory.createClass(
+              '<div class="b-balloon i-' + buildingType + '"><div class="b-balloon__close"></div><div class="b-balloon__icon" style="background-image: url(\'$[properties.src]\');"></div><div class="b-balloon__heading">$[properties.heading]</div></div>',
+            
+              balloonProps
+            );
+            
+            placemarks[ buildingType ] = [];
+            
+            for ( var w = 0; w < window.planPlacemarks[ buildingType ].length; w++ ) {
+              
+              buildingObj = window.planPlacemarks[ buildingType ][w];
+              
+              placemarks[ buildingType ][w] = new ymaps.Placemark(
+                buildingObj.coords,
+                {
+                  name: buildingObj.num,
+                  heading: buildingObj.name,
+                  src: buildingObj.src
+                },
+                {
+                  iconLayout: placemarkLayouts[ buildingType ],
+                  iconShape: {type: 'Rectangle', coordinates: [[-16,-16], [16,16]]},
+                  balloonShadow: false,
+                  balloonLayout: balloonLayouts[ buildingType ]
+                }
+              );
+              
+            }
+            
+          } else {
+            //create placemark layout
+            placemarkLayouts[ buildingType ] = ymaps.templateLayoutFactory.createClass( '<div class="b-placemark-icon i-' + buildingType + '">$[properties.name]</div>' );
+            
+            //create balloon layout
+            balloonLayouts[ buildingType ] = ymaps.templateLayoutFactory.createClass(
+              '<div class="b-balloon i-' + buildingType + '"><div class="b-balloon__close"></div><div class="b-balloon__name">$[properties.name]</div><div class="b-balloon__heading">$[properties.heading]</div><div class="b-balloon__props">$[properties.props]</div><div class="b-balloon__button"><a href="$[properties.href]">Все предложения</a></div></div>',
+            
+              balloonProps
+            );
+            
+            //create placemarks array
+            placemarks[ buildingType ] = [];
+            
+            for ( var i = 0; i < window.planPlacemarks[ buildingType ].length; i++ ) {
+              
+              buildingObj = window.planPlacemarks[ buildingType ][i];
+              
+              placemarks[ buildingType ][i] = new ymaps.Placemark(
+                buildingObj.coords,
+                {
+                  name: buildingObj.num,
+                  heading: buildingObj.heading,
+                  props: buildingObj.props,
+                  href: buildingObj.href
+                },
+                {
+                  iconLayout: placemarkLayouts[ buildingType ],
+                  iconShape: {type: 'Rectangle', coordinates: [[-16,-16], [16,16]]},
+                  balloonShadow: false,
+                  balloonLayout: balloonLayouts[ buildingType ]
+                }
+              );
+              
+            }
+          
+          }
+            
+          //create clusterer
+          clusterers[ buildingType ] = new ymaps.Clusterer({
+            preset: 'twirl#invertedVioletClusterIcons',
+            groupByCoordinates: false
+          });
+            
+          clusterers[ buildingType ].add( placemarks[ buildingType ]);
+            
+          //projectsYmap.geoObjects.add( clusterers[ buildingType ] );
+          for ( var q = 0; q < placemarks[ buildingType ].length; q++ ) {
+            projectsYmap.geoObjects.add( placemarks[ buildingType ][q] );
+          }
+          
+        }
+      }
+      
+      showHideInfrastructure();
+      
+      function showHideInfrastructure() {
+        var state = projectsYmap.action.getCurrentState();
+          
+        if ( state.zoom < 17 ) {
+          $( '#projectsYmap' ).addClass( 'i-show-infrastructure' );
+        } else {
+          $( '#projectsYmap' ).removeClass( 'i-show-infrastructure' );
+        }
+      }
+        
     });
     
   });
