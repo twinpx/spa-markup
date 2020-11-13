3cecf24298de7fa2603e2dd9e9962ab9c83110b3
Tatiana <djembuka@gmail.com>
2020-11-13 17:07:53 +0300

    Filter opacity

--- source/components/filter/script.js
+++ source/components/filter/script.js
@@ -3,93 +3,18 @@
   'use strict';
   
   $( function() {
-
-    //select
-    $( '.b-filter select' ).ikSelect();
     
-    //lazyload
-    $( '.b-flat-card__img' ).lazyload();
-    
-    //show result
-    $( '.b-filter-result' ).find( '.b-filter-result__block:last' ).slideDown();
+    //if it is the second file on the page
+    if ( document.querySelectorAll( '.b-filter' )[0].getAttribute( 'data-init' ) === 'true' ) {
+      return;
+    }
 
-    //flat click
-    $( '.b-filter-result' )
-    .delegate( '.b-flat-card', 'click', function() {
-      window.location = this.getAttribute( 'data-href' );
-    })
-    .delegate( '.b-flat-card__project a', 'click', function(e) {
-      e.stopPropagation();
-    });
-
-
-
-    //change the filter - set page opacity
-    document.querySelectorAll( '.b-filter input[type=checkbox]' ).forEach( function( elem ) {
-      elem.addEventListener( 'change', function() {
-        setPageOpacity();
-      });
-    });
-
-    $( '.b-filter' ).delegate( 'select', 'change', function() {
-      setPageOpacity();
-    });
-
+    //remove opacity by click on the body
     $( 'body' ).click( function(e) {
       if ( !$( 'body' ).hasClass( 'opacity' ) || e.target.closest( '.b-filter' )) {
         return;
       }
       removePageOpacity();
-    });
-
-
-
-    //submit
-    var moreButtonFlag = false;
-    $( '.b-filter form[ data-ajax=true ]' ).submit( function(e) {
-
-      e.preventDefault();
-
-      var $form = $( this );
-      var $filter = $form.closest( '.b-filter' );
-
-      $.ajax({
-        url: $form.attr( 'action' ),
-        type: $form.attr( 'method' ),
-        dataType: "html",
-        data: $form.serialize(),
-        success: function( html ) {
-          removePageOpacity();
-          if ( moreButtonFlag ) {
-            $( '.b-filter-result' ).append( html ).find( '.b-filter-result__block:last' ).slideDown();
-          } else {
-            $( '.b-filter-result' ).html( html ).find( '.b-filter-result__block:last' ).slideDown();
-          }
-          $( '.b-filter-result .b-filter-result__block:last .b-flat-card__img' ).lazyload();
-          moreButtonFlag = false;
-        },
-        error: function( a, b, c ) {
-          if ( window.console ) {
-            console.log(a);
-            console.log(b);
-            console.log(c);
-          }
-        }
-      });
-
-    });
-
-    //more
-    $( '.b-filter-result' ).delegate( '.b-filter-result__more .btn', 'click', function(e) {
-      e.preventDefault();
-
-      //increase the page num
-      document.querySelector( '.b-filter input[ name=page ]' ).value = $( this ).data( 'page' );
-      $( this ).closest( '.b-filter-result__more' ).remove();
-
-      //download
-      moreButtonFlag = true;
-      document.querySelector( '.b-filter form' ).requestSubmit();
     });
 
     Number.prototype.format = function(){
@@ -99,94 +24,193 @@
     String.prototype.deformat = function(){
       return Number( this.toString().split( ' ' ).join(''));
     };
-
-
-
-
-    //set sliders
-    [ 'square', 'price' ].forEach( function( elem ) {
+    
+    //script for each filter on the page
+    document.querySelectorAll( '.b-filter' ).forEach( function( filter ) {
       
-      var $range = $( '.b-filter__' + elem + '-range' );
-      var min = 1 * $range.data( 'min' );
-      var max = 1 * $range.data( 'max' );
-      var inputMin = document.querySelector( '.b-filter__' + elem + '-min' );
-      var inputMax = document.querySelector( '.b-filter__' + elem + '-max' );
-
-      //format input values
-      inputMin.value = Number( inputMin.value ).format();
-      inputMax.value = Number( inputMax.value ).format();
+      filter.setAttribute( 'data-init', 'true' );
       
-      //range
-      $range.slider({
-        animate: 'slow',
-        range: true,
-        min: min,
-        max: max,
-        values: [ String( inputMin.value ).deformat(), String( inputMax.value ).deformat() ],
-        slide: function( event, ui ) {
-          changeInput( ui.values, min, max, [ inputMin, inputMax ], $range );
-        },
-        change: function() {
-          setPageOpacity();
-        }
-      });
+      var $filter = $( filter );
       
-      //min
-      $( inputMin )
+      //select
+      $filter.find( 'select' ).ikSelect();
       
-      .blur( function(e) {
+      //show result
+      var filterResultBlock = filter.closest( '.b-filter-result-block' ),
+          filterResult, $filterResult;
+      
+      if ( filterResultBlock ) {
         
-        if ( e.which === '13' ) {
+        filterResult = filterResultBlock.querySelector( '.b-filter-result' );
+        $filterResult = $( filterResult );
+        
+        $filterResult.find( '.b-filter-result__block:last' ).slideDown();
+      
+        //lazyload
+        $filterResult.find( '.b-flat-card__img' ).lazyload();
+        
+        //flat click
+        $filterResult
+        .delegate( '.b-flat-card', 'click', function() {
+          window.location = this.getAttribute( 'data-href' );
+        })
+        .delegate( '.b-flat-card__project a', 'click', function(e) {
           e.stopPropagation();
-          e.preventDefault();
-        }
-        
-        changeInput( [this.value], min, max, [ inputMin, inputMax ], $range );
-      })
+        });
       
-      .keydown( function(e) {
-        if ( e.which === 13 ) {
-          return false;
-        }
-        setPageOpacity();
-      })
-      .keyup( function(e) {
-        if ( e.which === 13 ) {
-          return false;
-        } else {
-          inputMin.value = Number( String( inputMin.value ).deformat()).format();
-        }
-        setPageOpacity();
+        //more
+        $filterResult.delegate( '.b-filter-result__more .btn', 'click', function(e) {
+          e.preventDefault();
+
+          //increase the page num
+          filter.querySelector( 'input[ name=page ]' ).value = $( this ).data( 'page' );
+          $( this ).closest( '.b-filter-result__more' ).remove();
+
+          //download
+          moreButtonFlag = true;
+          filter.querySelector( 'form' ).requestSubmit();
+        });
+      
+      }
+      
+      //change the filter - set page opacity
+      filter.querySelectorAll( 'input[type=checkbox]' ).forEach( function( elem ) {
+        elem.addEventListener( 'change', function() {
+          setPageOpacity( filter );
+        });
       });
       
-      //max
-      $( inputMax )
-      
-      .blur( function(e) {
-        
-        if ( e.which === '13' ) {
-          e.stopPropagation();
-          e.preventDefault();
-        }
-        
-        changeInput( [undefined, this.value], min, max, [ inputMin, inputMax ], $range );
-      })
-      
-      .keydown( function(e) {
-        if ( e.which === 13 ) {
-          return false;
-        }
-        setPageOpacity();
-      })
-      .keyup( function(e) {
-        if ( e.which === 13 ) {
-          return false;
-        } else {
-          inputMax.value = Number( String( inputMax.value ).deformat() ).format();
-        }
-        setPageOpacity();
+      $filter.delegate( 'select', 'change', function() {
+        setPageOpacity( filter );
       });
       
+      //submit
+      var moreButtonFlag = false;
+      $filter.find( 'form[ data-ajax=true ]' ).submit( function(e) {
+
+        e.preventDefault();
+
+        var $form = $( this );
+        $form.addClass( 'disabled' );
+
+        $.ajax({
+          url: $form.attr( 'action' ),
+          type: $form.attr( 'method' ),
+          dataType: "html",
+          data: $form.serialize(),
+          success: function( html ) {
+            
+            $form.removeClass( 'disabled' );
+            
+            removePageOpacity();
+            if ( moreButtonFlag ) {
+              $filterResult.append( html ).find( '.b-filter-result__block:last' ).slideDown();
+            } else {
+              $filterResult.html( html ).find( '.b-filter-result__block:last' ).slideDown();
+            }
+            $filterResult.find( '.b-filter-result__block:last .b-flat-card__img' ).lazyload();
+            moreButtonFlag = false;
+          },
+          error: function( a, b, c ) {
+            
+            $form.removeClass( 'disabled' );
+            
+            if ( window.console ) {
+              console.log(a);
+              console.log(b);
+              console.log(c);
+            }
+          }
+        });
+
+      });
+      
+      //set sliders
+      [ 'square', 'price' ].forEach( function( elem ) {
+        
+        var $range = $filter.find( '.b-filter__' + elem + '-range' );
+        var min = 1 * $range.data( 'min' );
+        var max = 1 * $range.data( 'max' );
+        var inputMin = filter.querySelector( '.b-filter__' + elem + '-min' );
+        var inputMax = filter.querySelector( '.b-filter__' + elem + '-max' );
+
+        //format input values
+        inputMin.value = Number( inputMin.value ).format();
+        inputMax.value = Number( inputMax.value ).format();
+        
+        //range
+        $range.slider({
+          animate: 'slow',
+          range: true,
+          min: min,
+          max: max,
+          values: [ String( inputMin.value ).deformat(), String( inputMax.value ).deformat() ],
+          slide: function( event, ui ) {
+            changeInput( ui.values, min, max, [ inputMin, inputMax ], $range );
+          },
+          change: function() {
+            setPageOpacity( filter );
+          }
+        });
+        
+        //min
+        $( inputMin )
+        
+        .blur( function(e) {
+          
+          if ( e.which === '13' ) {
+            e.stopPropagation();
+            e.preventDefault();
+          }
+          
+          changeInput( [this.value], min, max, [ inputMin, inputMax ], $range );
+        })
+        
+        .keydown( function(e) {
+          if ( e.which === 13 ) {
+            return false;
+          }
+          setPageOpacity( filter );
+        })
+        .keyup( function(e) {
+          if ( e.which === 13 ) {
+            return false;
+          } else {
+            inputMin.value = Number( String( inputMin.value ).deformat()).format();
+          }
+          setPageOpacity( filter );
+        });
+        
+        //max
+        $( inputMax )
+        
+        .blur( function(e) {
+          
+          if ( e.which === '13' ) {
+            e.stopPropagation();
+            e.preventDefault();
+          }
+          
+          changeInput( [undefined, this.value], min, max, [ inputMin, inputMax ], $range );
+        })
+        
+        .keydown( function(e) {
+          if ( e.which === 13 ) {
+            return false;
+          }
+          setPageOpacity( filter );
+        })
+        .keyup( function(e) {
+          if ( e.which === 13 ) {
+            return false;
+          } else {
+            inputMax.value = Number( String( inputMax.value ).deformat() ).format();
+          }
+          setPageOpacity( filter );
+        });
+        
+      });
+    
     });
     
     function changeInput( valuesArray, min, max, inputArray, $range ) {
@@ -214,10 +238,8 @@
         if ( valuesArray[1] > max || valuesArray[1] < String( inputArray[0].value ).deformat() ) {
           valuesArray[1] = max;
         }
-        
 
       }
-      
       
       valuesArray.forEach( function( value, index ) {
 
@@ -237,11 +259,9 @@
 
       });
 
-      
-
     }
     
-    function setPageOpacity() {
+    function setPageOpacity( filter ) {
 
       if ( document.querySelector( 'body' ).getAttribute( 'class' ) && document.querySelector( 'body' ).getAttribute( 'class' ).search( 'opacity' ) !== -1 ) {
         return;
@@ -249,7 +269,7 @@
       
       document.querySelector( 'body' ).classList.add( 'opacity' );
 
-      setClear( document.querySelector( '.b-filter' ));
+      setClear( filter );
 
       function setClear( elem ) {
         if ( elem.getAttribute( 'class' ).search( 'b-page-content' ) !== -1 || elem.tagName.toLowerCase === 'body' ) {
